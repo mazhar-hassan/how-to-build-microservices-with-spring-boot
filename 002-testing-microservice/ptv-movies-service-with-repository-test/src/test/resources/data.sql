dc4f12507ebb8ea3ab3204bdf49b72f4e9a4680c
Mazhar Hassan <netsurgeon@gmail.com>
2020-12-31 01:43:57 +0500

    Added test for repository
    1 - Added h2 database pom dependency for test
    2 - Configured h2 database in application.application.properties of test
    3 - Added schema.sql to create schema before running tests
    4 - Added data.sql to insert initial data before running tests
    5 - Created integration test for Repository

---
+++ 002-testing-microservice/ptv-movies-service-with-repository-test/src/test/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO tv_movies
+(id, name, description)
+VALUES
+(1, 'Titanic - Test Movie', 'The ship movie'),
+(2, 'Predator - Test Movie', 'Horror movie'),
+(3, 'The Last Samurai - Test Movie', 'Code and Honour'),
+(4, 'Star wars - Test Movie', 'Space star wars'),
+(5, 'Rambo 3 - Test Movie', 'A tribute to gallant people of Afghanistan');
